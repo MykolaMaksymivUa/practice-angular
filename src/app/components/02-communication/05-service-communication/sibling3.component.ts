e72d80194b422b9d4a5dece15c5cf7e22904ce9c
Mykola_Maksymiv <Mykola_Maksymiv@epam.com>
2020-03-19 21:22:27 +0200

    test

--- src/app/components/02-communication/05-service-communication/sibling3.component.ts
+++ src/app/components/02-communication/05-service-communication/sibling3.component.ts
@@ -8,10 +8,35 @@ import { DataService } from './data.service';
     <p>
       sibling3
       <button [disabled]="disabled" class="btn btn-primary">Some Btn</button>
-      <input [attr.type]="inputType" [class]="'myClass'" />
+      <input [attr.type]="inputType" [ngClass]="setClasses()" />
       <button class="btn btn-primary" (click)="onClick()">Pass Data</button>
     </p>
-  `
+  `,
+  styles: [
+    `.myClass1 {
+      display: block;
+      height: 30px;
+      width: 1000px;
+      background-color: blue;
+      border: unset;
+    }
+
+    .myClass2 {
+      display: block;
+      height: 30px;
+      width: 1000px;
+      background-color: yellow;
+      border: unset;
+    }
+    .someClass1 {
+      margin: 5px;
+    }
+
+    .someClass2 {
+      border-top: 2px solid black;
+    }
+    `,
+  ]
 })
 export class Sibling3Component {
   counter = 0;
@@ -27,5 +52,16 @@ export class Sibling3Component {
   //change detection.
   get inputType(): string {
     return this.disabled ? 'text': 'password';
+  }
+
+  setClasses(): any {
+    let classes =  {
+      someClass1: this.counter % 3,
+      someClass2: this.counter % 2,
+      myClass1: this.disabled,
+      myClass2: !this.disabled,
+    }
+
+    return classes;
   }
 }
