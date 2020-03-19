01120998ab5a46db7057f3c8381bf6b5400d8a95
Mykola Maksymiv <mykolamaksymivua@gmail.com>
2020-03-19 17:04:21 +0200

    debugging

--- src/app/components/02-communication/05-service-communication/sibling3.component.ts
+++ src/app/components/02-communication/05-service-communication/sibling3.component.ts
@@ -7,17 +7,25 @@ import { DataService } from './data.service';
   template: `
     <p>
       sibling3
+      <button [disabled]="disabled" class="btn btn-primary">Some Btn</button>
+      <input [attr.type]="inputType" [class]="'myClass'" />
       <button class="btn btn-primary" (click)="onClick()">Pass Data</button>
     </p>
   `
 })
 export class Sibling3Component {
   counter = 0;
-
+  disabled: boolean = true;
   constructor(private dataService: DataService<string>) {}
 
   onClick() {
     this.counter++;
+    this.disabled = !this.disabled;
     this.dataService.setData(`Data from sibling 1(${this.counter})`);
+  }
+
+  //change detection.
+  get inputType(): string {
+    return this.disabled ? 'text': 'password';
   }
 }
